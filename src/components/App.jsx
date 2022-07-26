5396f0e52d1d903744dc77021f90d85a7f0c89f9
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-07-26 18:13:40 +0300

    rewritten to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import {useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import Message from './Message';
+import css from './App.module.css';
+
+const App = () => {
+  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? '');
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    console.log( 'dadsfd',localStorage.setItem('contacts', JSON.stringify(contacts)));
+  }, [contacts]);
+
+
+  const addContact = ({ name, number }) => {
+    const newContact = { id: nanoid(), name, number };
+
+    contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
+      ? Report.warning(
+          `${name}`,
+          'This user is already in the contact list.',
+          'OK'
+        )
+      : setContacts(prevContacts => [newContact, ...prevContacts]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts => prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+   const filtredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+    return (
+      <div className={css.container}>
+        <h1 className={css.title}>
+          Phone<span className={css.title__color}>book</span>
+        </h1>
+        <ContactForm onSubmit={addContact} />
+
+        <h2 className={css.subtitle}>Contacts</h2>
+        <Filter filter={filter} changeFilter={changeFilter} />
+        
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={filtredContacts()}
+            onDeleteContact={deleteContact}
+          />
+        ) : (
+          <Message text="Contact list is empty." />
+        )}
+        
+      </div>
+    );
+  }
+
+
+export default App;
