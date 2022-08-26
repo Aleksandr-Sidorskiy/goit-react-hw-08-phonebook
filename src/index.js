2a8e83e98c1e8bfaca433a4bd38264876a53f05f
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-08-26 21:48:22 +0300

    start hw-8

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import  App  from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-// import { PersistGate } from 'redux-persist/es/integration/react';
+import {
+  store, //persistor 
+} from './redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <PersistGate loading={null} persistor={persistor}> */}
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </Provider>
     {/* </PersistGate> */}
     
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
 import { contactApi } from './contactApi';
 import  {contactsSlice}  from './contactsSlice';
 
-
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 export const store = configureStore({
   reducer: {
